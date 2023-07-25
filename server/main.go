4b63da06a4142889e20990a7d812ac85aaa561e4
Shubham Singh <shubhammahar1306@gmail.com>
2023-07-25 10:25:42 +0530

    server part done, main.go ready

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"os"
 	"github.com/gin-contrib/cors"
+	"github.com/1shubham7/calory-count/server/routes"
 )
 
 func main() {
@@ -15,4 +16,15 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(cors.Default)
+
+	router.POST("/entry/create", routes.AddEntry)
+	router.GET("/entries", routes.GetEntries)
+	router.GET("/entry/:id/", router.EntryById)
+	router.GET("/ingredient/:ingredients", routes.GetEntriesByIngredient)
+
+	router.PUT("entry/update/:id", routes.UpdateEntry)
+	router.PUT("ingredient/update/:id", routes.UpdateIngredient)
+	router.DELETE("entry/delete/:id", routes.DeleteEntry)
+
+	router.Run(":" + port)
 }
