2f589a2c17b9504736a7636708fc0a7b863d13e1
Shubham Singh <shubhammahar1306@gmail.com>
2023-07-25 10:04:31 +0530

    main.go

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/1shubham7/calory-count
 
 go 1.20
+
+require go.mongodb.org/mongo-driver v1.12.0
--- server/main.go
+++ server/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"os"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
-    
+    port := os.Getenv("PORT")
+	if port == "" {
+		port = "6969"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(cors.Default)
 }
